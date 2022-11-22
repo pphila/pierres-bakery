6cdb9590e37e752cd7ed0568b19dd15cc2c15471
pphila <efl2ice89@gmail.com>
2022-11-22 13:14:39 -0800

    add pass to bread total calculator test

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -12,5 +12,22 @@ namespace Bakery.Models
       BreadType = breadType; 
       BreadQuantity = breadQuantity;
     }
+
+    public int CalculatedBreadCost(int breadQuantity)
+    {
+      int breadTotal = 0;
+      
+      if(breadQuantity >= 3)
+      {
+        int qauntityCheck = breadQuantity / 3;
+        breadTotal += 10 * qauntityCheck;
+        breadQuantity -= (qauntityCheck * 3);
+      }
+      else
+      {
+        breadTotal = breadQuantity * 5;
+      }
+      return breadTotal;
+    }
   }
 }
