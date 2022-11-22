4b24aef9f454ff3d77c6839410762187cf9b9bb4
pphila <efl2ice89@gmail.com>
2022-11-22 11:32:06 -0800

    add passed second test testing for the getter

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -6,11 +6,27 @@ namespace Bakery.Tests
   [TestClass]
   public class BreadTests
   {
+
     [TestMethod]
     public void BreadClass_CreatesInstanceOfBread_Bread()
     {
       Bread breadOrder = new Bread("testBread", 2);
       Assert.AreEqual(typeof(Bread), breadOrder.GetType());
+    }
+
+    [TestMethod]
+    public void GetBreadType_ReturnBreadType_Sting()
+    {
+      //Arrange
+      string breadType = "rye";
+      int breadQuantity = 2;
+      Bread breadOrder = new Bread(breadType, breadQuantity);
+      
+      //Act
+      string result = breadOrder.BreadType;
+
+      //Assert
+      Assert.AreEqual(breadType, result);
     }
   }
 }
