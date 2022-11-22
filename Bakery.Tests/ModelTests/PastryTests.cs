ec3e1879181446f15aa71cce436a41c31f407a6f
pphila <efl2ice89@gmail.com>
2022-11-22 14:12:52 -0800

    add second, third, and fourth test using different integer amounts for pastry calculator test passing all tests

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -75,5 +75,50 @@ namespace Bakery.Tests
       //Assert
       Assert.AreEqual(5, result);
     }
+
+    [TestMethod]
+    public void Second_GetCalculatedPastryCost_Int()
+    {
+      //Arrange
+      string pastryType = "cannoli";
+      int pastryQuantity = 4;
+      Pastry pastryOrder = new Pastry(pastryType, pastryQuantity);
+
+      //Act
+      int result = pastryOrder.CalculatedPastryCost(pastryQuantity);
+
+      //Assert
+      Assert.AreEqual(7, result);
+    }
+    
+    [TestMethod]
+    public void Third_GetCalculatedPastryCost_Int()
+    {
+      //Arrange
+      string pastryType = "kringle";
+      int pastryQuantity = 5;
+      Pastry pastryOrder = new Pastry(pastryType, pastryQuantity);
+
+      //Act
+      int result = pastryOrder.CalculatedPastryCost(pastryQuantity);
+
+      //Assert
+      Assert.AreEqual(9, result);
+    }
+    
+    [TestMethod]
+    public void Fourth_GetCalculatedPastryCost_Int()
+    {
+      //Arrange
+      string pastryType = "macaron";
+      int pastryQuantity = 6;
+      Pastry pastryOrder = new Pastry(pastryType, pastryQuantity);
+
+      //Act
+      int result = pastryOrder.CalculatedPastryCost(pastryQuantity);
+
+      //Assert
+      Assert.AreEqual(10, result);
+    }
   }
 }
