8e67fe29ae4641b78d11724ef75429d5c6ed8a9f
pphila <efl2ice89@gmail.com>
2022-11-22 11:12:48 -0800

    add redphase to first test

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -5,4 +5,12 @@ namespace Bakery.Tests
 {
   [TestClass]
   public class BreadTests
+  {
+    [TestMethod]
+    public void BreadClass_CreatesInstanceOfBread_Bread()
+    {
+      Bread newBread = new Bread("testBread", 2);
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+  }
 }
